f7c54bd2388043d0ee44cb8e82081d2f09d82805
ajitsingh <jeet.singh.2689@gmail.com>
2019-11-12 14:04:04 +0530

    File Update

---
+++ app/src/main/java/com/justapptechnology/mydesignpattern/DetatlItemActivity.java
@@ -0,0 +1,15 @@
+package com.justapptechnology.mydesignpattern;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetatlItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details_item_activity);
+    }
+}
